dd46c4f15c4ce5af23536c9a9a4f879c19e1ffa2
CHABUS Océane <chabus.oceane@hotmail.com>
2023-03-17 20:30:42 +0100

    Modif pour chargement

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const fs=require("fs").promises
+const fs = require("fs").promises
 const express = require('express');
 let world = require("./world");
 
@@ -10,29 +10,30 @@ const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 
 const server = new ApolloServer({
-    typeDefs, resolvers ,context: async ({ req }) => ({
+    typeDefs, resolvers, context: async ({ req }) => ({
         world: await readUserWorld(req.headers["x-user"]),
         user: req.headers["x-user"]
     })
 });
 const app = express();
+app.use(cors());
+
 app.use(express.static('public'));
 server.start().then(res => {
     server.applyMiddleware({ app });
     app.listen({ port: 4000 }, () =>
-        console.log(`🚀 Server ready at
-http://localhost:4000${server.graphqlPath}`)
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
     );
 
 })
 
 async function readUserWorld(user) {
     try {
-    const data = await fs.readFile("userworlds/"+ user + "-world.json");
-    return JSON.parse(data);
+        const data = await fs.readFile("userworlds/" + user + "-world.json");
+        return JSON.parse(data);
     }
-    catch(error) {
-    return world
+    catch (error) {
+        return world
     }
-   }
-   
+}
+
